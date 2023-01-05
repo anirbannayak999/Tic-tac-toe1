730785a5bbb77ae78d781b541ac234864dd9a397
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-05 18:09:39 +0530

    uc2

--- src/com/bridgelabz/uc1_createaboardgame.java
+++ src/com/bridgelabz/uc1_createaboardgame.java
@@ -1,9 +1,28 @@
 package com.bridgelabz;
 
 public class uc1_createaboardgame {
-    public static void main(String[] args) {
-        System.out.println("welcome to tictactoe programe");
+
+
+    static char [] board = new char[10];
+
+    static void createBoard() {
+        for(int i=1; i<board.length; i++) {
+            board[i] = ' ';
+        }
+        System.out.println("board is created");
     }
 
 
-}
+
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("welcome to tictactoe programe");
+        createBoard();
+
+
+        }
+    }
